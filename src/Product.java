84ede05b85f04d63391a4be4f67ff272288d0070
alirt <alirtaskiran@gmail.com>
2022-09-25 15:09:18 +0300

    Contructor oluşturuldu.

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,13 @@
 public class Product {
+    public Product(int id, String name, String description, double price, int stockAmount, String color) {
+        System.out.println("Yapici blok calisti...");
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.stockAmount = stockAmount;
+        this.description = description;
+    }
+
     private int id;
     private String name;
     private String description;
@@ -60,7 +69,7 @@ public class Product {
     }
 
     public String getCodeNumber() {
-        return this.name.substring(0,1) + id;
+        return this.name.substring(0, 1) + id;
     }
 
     public void setCodeNumber(String codeNumber) {
